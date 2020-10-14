dbf199fb4bd52fc1d2137c5463401df9570c90e4
ravana99 <tomas.lopes@tecnico.ulisboa.pt>
2020-10-14 21:56:53 +0100

    clean up prints, add more comments, remove outdated todo

--- proj1.py
+++ proj1.py
@@ -16,7 +16,7 @@ from whoosh.query import Every       # for testing, to retrieve every document
 # Customize parameters here:
 
 corpus_dir = "../material/rcv1"     # Directory of your rcv1 folder
-docs_to_index = 1000                # How many docs to add to index, set to None to add all of the docs in the corpus
+docs_to_index = 250                 # How many docs to add to index, set to None to add all of the docs in the corpus
 
 #######################################################################################################################
 
@@ -24,7 +24,7 @@ docs_to_index = 1000                # How many docs to add to index, set to None
 def indexing(corpus):  # TODO: allow customization for text preprocessing using extra function arguments
     start_time = time.time()
 
-    # TODO: split content according to XML tags and DON'T STORE THE CONTENT IN THE INDEX!! That is just for testing
+    # TODO: split content according to XML tags
     schema = Schema(id=NUMERIC(stored=True), content=TEXT)
 
     # Clear existing indexdir folder and make new one
@@ -40,6 +40,7 @@ def indexing(corpus):  # TODO: allow customization for text preprocessing using 
 
     end_time = time.time()
 
+    # Traverses all files in the indexdir folder to calculate disk space taken up by the index
     space = 0
     for subdir, dirs, files in os.walk("indexdir"):
         space += sum(os.stat(os.path.join("indexdir", file)).st_size for file in files)
@@ -47,10 +48,12 @@ def indexing(corpus):  # TODO: allow customization for text preprocessing using 
     return ix, end_time - start_time, space
 
 
+# Traverses all sub-folders/files in the corpus and adds every document to the index
 def traverse_folders(writer, corpus):
     n_docs = 0
     for subdir, dirs, files in os.walk(corpus):
         for file in files:
+            # Ignore non-document files
             if file != "MD5SUMS" and subdir != os.path.join(corpus, "codes") and subdir != os.path.join(corpus, "dtds"):
                 with open(os.path.join(subdir, file), "r") as f:
                     writer.add_document(id=file[:-10], content=f.read())
@@ -58,10 +61,19 @@ def traverse_folders(writer, corpus):
                     return
 
 
+def convert_filesize(size):
+    suffixes = ("B", "KiB", "MiB", "GiB", "TiB")
+    i = 0
+    while size // 1024 > 0 and i < 4:
+        size /= 1024.0
+        i += 1
+    return str(round(size, 3)) + " " + suffixes[i]
+
+
 def main():
-    (ix, ix_time, ix_memory) = indexing(corpus_dir)
-    print(f"Time to build index: {ix_time} seconds")
-    print(f"Disk space taken up by the index: {ix_memory} bytes")
+    (ix, ix_time, ix_space) = indexing(corpus_dir)
+    print(f"Time to build index: {round(ix_time, 3)}s")
+    print(f"Disk space taken up by the index: {convert_filesize(ix_space)}")
 
     with ix.searcher() as searcher:
         results = searcher.search(Every(), limit=None)  # Returns every document
