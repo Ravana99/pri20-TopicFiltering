c34e8da8a39a1eb43d7003d63387c269531e9329
ravana99 <tomas.lopes@tecnico.ulisboa.pt>
2020-10-17 17:07:52 +0100

    add basic version of extract_topic_query

--- proj1.py
+++ proj1.py
@@ -1,6 +1,12 @@
 import os
 import shutil
 import time
+import re
+import math
+import xml.etree.ElementTree as et
+
+from collections import Counter
+from whoosh import scoring
 from whoosh.index import create_in
 from whoosh.fields import *
 
@@ -57,10 +63,65 @@ def traverse_folders(writer, corpus):
         for file in files:
             # Ignore non-document files
             if file != "MD5SUMS" and subdir != os.path.join(corpus, "codes") and subdir != os.path.join(corpus, "dtds"):
-                with open(os.path.join(subdir, file), "r") as f:
-                    writer.add_document(id=file[:-10], content=f.read())
+                doc, doc_id = extract_doc_content(os.path.join(subdir, file))
+                writer.add_document(id=doc_id, content=doc)
                 if (n_docs := n_docs + 1) == docs_to_index:
                     return
+
+
+def extract_doc_content(file):
+    tree = et.parse(file)
+    root = tree.getroot()
+    res = root.attrib["date"] + " "
+    doc_id = int(root.attrib["itemid"])
+    for child in root:
+        if child.tag in ("headline", "dateline", "byline"):
+            res += child.text + " "
+        elif child.tag == "text":
+            for paragraph in child:
+                res += paragraph.text + " "
+    return res, doc_id
+
+
+def extract_topic_query(topic_id, index, k):
+    topic_id = int(topic_id)-101       # Normalize topic identifier to start at 0
+    with open(os.path.join(corpus_dir, "..", "topics.txt")) as f:
+        topics = f.read().split("</top>")
+    topic = topics[topic_id]
+    topic = re.sub("<num> Number: R[0-9][0-9][0-9]", "", topic)
+    for tag in ("<top>", "<title>", "<desc> Description:", "<narr> Narrative:"):
+        topic = topic.replace(tag, "")
+
+    schema = Schema(id=NUMERIC(stored=True), content=TEXT)
+
+    if os.path.exists("topicindexdir"):
+        shutil.rmtree("topicindexdir")
+    os.makedirs("topicindexdir")
+
+    aux_index = create_in("topicindexdir", schema)
+    writer = aux_index.writer()
+    writer.add_document(id=0, content=topic)
+    writer.commit()
+
+    # TODO: deal with words that show up in all the topics (e.g. "documents")
+    with aux_index.searcher() as aux_searcher:
+        # Dictionary of term frequencies in the TOPIC
+        tf_dic = {word.decode("utf-8"): aux_searcher.frequency("content", word)
+                  for word in aux_searcher.lexicon("content")}
+        n_terms_in_topic = sum(tf_dic.values())
+        tf_dic = {word: freq/n_terms_in_topic for word, freq in tf_dic.items()}
+
+        with index.searcher() as searcher:
+            # Dictionary of document frequencies of each term against the DOCUMENT INDEX
+            results = searcher.search(Every(), limit=None)  # Returns every document
+            n_docs = len(results)
+            df_dic = {word.decode("utf-8"): searcher.doc_frequency("content", word)
+                      for word in aux_searcher.lexicon("content")}
+            idf_dic = {word: math.log10(n_docs/(df+1)) for word, df in df_dic.items()}
+
+    tfidfs = {key: tf_dic[key] * idf_dic[key] for key in tf_dic}
+
+    print(list(tup[0] for tup in Counter(tfidfs).most_common(k)))
 
 
 # Prints the entire index for debugging and manual analysis purposes
@@ -97,6 +158,8 @@ def main():
         print(f"Number of docs returned: {len(results)}")
         # print(f"Docs returned: { {r['id']: r['content'] for r in results} }")
         # print(f"Doc scores: { {r['id']: r.score for r in results} }")  # Scores are always 1.0 with an Every() query
+
+    extract_topic_query(101, ix, 5)
 
 
 main()
